f12606cf48b07b8e408b585d8ff1bcb55d21ab38
ValentinAlekhin <headbangernetwork@gmail.com>
2020-04-19 11:45:10 +0300

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = config.get('port') || 5000
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUrl'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+
+    app.listen(PORT, () => console.log(`Server has been sterted on port: ${PORT}`))
+  } catch (err) {
+    console.log('Server error:', err)
+    process.exit(1)
+  }
+}
+
